5f7307d80aff765f03ae9726a32d5099b1565e94
Ambesh kumar <48892208+201851019-iiitv@users.noreply.github.com>
2020-08-04 23:35:11 +0530

    Added Main file
    this is c file

---
+++ guessing_game.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+ int main(int argc, char const *argv[]) {
+
+int number=1;
+srand(time(0));
+int n=(rand()%100)+1  ; //1----100
+int num;
+printf("Enter the Your Guess number Between 1 to 100 \n");
+do
+{
+
+
+scanf("%d",&num);
+
+if(num>n)
+printf(" Please Enter the lower number !! \n");
+else if(num<n)
+printf(" Please Enter the higher number !! \n");
+else
+{
+
+  printf(" Yes !! You guessed \n");
+  printf(" You guess in  %d ",number);
+  printf(" attempts !! \n");
+}
+
+
+number++;
+}while(n!=num);
+
+
+return 0;
+}
